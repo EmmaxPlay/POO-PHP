79286c0abf1bb67fb40abdfa148bb91d00843584
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-22 11:42:47 -0600

    Metodos

--- 03-metodos.php
+++ 03-metodos.php
@@ -3,9 +3,29 @@
 class MenuRestaurant {
     public $nombre = 'Nombre del Producto';
     public $precio = 0;
+
+    public function obtenerProductoyNombre(){
+        echo "el producto " . $this->nombre . "Tiene un precio de " . $this->precio;
+    }
 }
 
 
 // Instanciar una clase
 $bebida = new MenuRestaurant();
+$bebida->nombre = "jugo de naranja";
+$bebida->precio = 100;
+$bebida->obtenerProductoyNombre();
+
+echo "<br/>";
+
 $postre = new MenuRestaurant();
+$postre->nombre = "Pastel de chocolate";
+$postre->precio = 150;
+$postre->obtenerProductoyNombre();
+
+echo "<br/>";
+
+$corte = new MenuRestaurant();
+$corte->nombre = "Rib Eye ";
+$corte->precio = 750;
+$corte->obtenerProductoyNombre();
