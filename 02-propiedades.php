9fc57a50d077bcffb3b5cfafa618aa86691d91a4
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-22 11:26:02 -0600

    Propiedades y atributos

--- 02-propiedades.php
+++ 02-propiedades.php
@@ -1,6 +1,18 @@
 <?php include 'includes/header.php';
 
-class MenuRestaurant {   
-     
+class MenuRestaurant {
+    public $nombre = 'Nombre del producto';
+    public $precio = '0';
 }
-    
+
+$bebida = new MenuRestaurant();
+$bebida->nombre = "jugo de naranja";
+$bebida->precio = "$40";
+
+
+var_dump($bebida);
+
+$postre = new MenuRestaurant();
+$postre->nombre = "pastel";
+$postre->precio = "$100";
+var_dump($postre);
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +10,7 @@
 <header class="bg-blue-800">
     <h1 class="text-white text-center text-2xl bg-blue-900 p-3 uppercase font-bold">Aprendiendo Programación Orientada a Objetos en PHP</h1>
 
-    <div class=" p-5"> 
+    <div class=" p-5">
         <p class="text-blue-200 mb-2 text-center">Sígueme en Twitter: <a href="https://twitter.com/JuanDevWP" target="_blank" rel="nofollow noopener noreferrer" > https://twitter.com/JuanDevWP </p>
         <p class="text-blue-200 mb-2 text-center">Vísita mi Sitio Web: <a href="http://codigoconjuan.com/" target="_blank" rel="nofollow noopener noreferrer" > https://codigoconjuan.com </a></p>
     </div>
